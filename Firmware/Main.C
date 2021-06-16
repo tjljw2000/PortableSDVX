bc28920c41ea9548ad2a2ee876e96ba78d42b83e
Jiawei Li <u7096225@anu.edu.au>
2021-06-17 01:58:18 +0800

    Firmware updates.

--- Firmware/Main.C
+++ Firmware/Main.C
@@ -1,3 +1,4 @@
+#include <string.H>
 #include "CH552.H"
 #include "Debug.H"
 #include "USB.H"
@@ -17,18 +18,57 @@ sbit EC11_B = P1 ^ 3; // EC11的A、B脚
 sbit EC11_C = P3 ^ 4;
 sbit EC11_D = P3 ^ 5; // EC11的C、D脚
 
-main()
+UINT8 nkey = 0;
+UINT8 result = 0;
+UINT8 last_result = 0;
+
+void HIDValueHandleMouse()
+{
+    TR0 = 0;                     // 发送前关定时器中断
+    FLAG = 0;                    // 清空USB中断传输完成标志，准备发送按键按下数据
+    Enp1IntIn();                 // USB设备模式端点2的中断上传
+    while(FLAG == 0);            // 等待USB中断数据传输完成
+    TR0 = 1;                     // 发送完打开定时器中断
+}
+
+void keyboardUpKeyValue(UINT8 key)
+{
+    HIDKey[2] = key;
+    while ( (UEP1_CTRL & MASK_UEP_T_RES) == UEP_T_RES_ACK);
+    HIDValueHandleMouse();
+}
+
+int main()
 {
     CfgFsys();
-    mDelaymS(20);
+    mDelaymS(5);
     USBDeviceInit();
+    
     EA = 1;                                                               //允许单片机中断
     UEP1_T_LEN = 0;                                                       //预使用发送长度一定要清空
     UEP2_T_LEN = 0;                                                       //预使用发送长度一定要清空
     FLAG = 0;
     Ready = 0;
+
     while (1)
     {
-        
+        if (Ready) {
+            scan_key();
+            if (changed) {
+                keyboardUpload();
+            }
+            keyboardUpKeyValue(keymaps[1]);
+        } 
+        result = key1;
+        mDelaymS(5);
+        if (result == key1) {
+            if (!result)  nkey = keymaps[1];
+            else          nkey = 0;
+        }
+        if (last_result != result) {
+            last_result = result;
+            keyboardUpKeyValue(nkey);
+        }
+			keyboardUpKeyValue(keymaps[0]);
     }
 }
